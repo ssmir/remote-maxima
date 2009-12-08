8e8ca3707f7689e8841c95be5ab18d3789568332
Sergey Smirnov <sasmir@gmail.com>
2009-12-08 20:30:55 +0300

    Added JNI wrapper for MaximaInstance class.

---
+++ java/ru/isa/dcs/ssmir/maxima/MaximaException.java
@@ -0,0 +1,15 @@
+/*  Maxima JNI API
+    Copyright (C) 2007-2009 Sergey Smirnov <sasmir (at) gmail.com>
+    
+    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    file BOOST_LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+package ru.isa.dcs.ssmir.maxima;
+
+public class MaximaException extends Exception {
+	public MaximaException(String msg) {
+		super(msg);
+	}
+}
+
